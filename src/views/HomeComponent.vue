7623da757312396ffacb68488d213968a4faeec5
SimpleHacker409 <manjunath.siddesh@studenti.unimi.it>
2022-05-02 18:03:38 +0200

    Initial
    Signed-off-by: SimpleHacker409 <manjunath.siddesh@studenti.unimi.it>

---
+++ src/views/HomeComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+      <h1 class="text-2xl text-orange-600">{{ name }}</h1>
+      <h2 class="text-2xl">This is Home component</h2>
+  </div>
+</template>
+
+<script setup>
+import {computed} from 'vue'
+import {useStore} from 'vuex'
+
+const store = useStore()
+
+const name = computed(() => {
+    return store.state.user.name
+})
+
+
+
+</script>
